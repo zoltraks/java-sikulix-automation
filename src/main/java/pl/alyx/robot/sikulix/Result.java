d4929f0898f760b55a9453d154f2923336532795
Filip Golewski <f.golewski@gmail.com>
2023-01-21 22:17:13 +0100

    fixes, improvements, changes

---
+++ src/main/java/pl/alyx/robot/sikulix/Result.java
@@ -0,0 +1,11 @@
+package pl.alyx.robot.sikulix;
+
+public class Result {
+
+    public boolean success;
+
+    public String error;
+
+    public String jump;
+
+}
---
+++ src/main/java/pl/alyx/robot/sikulix/structure/Settings.java
@@ -0,0 +1,15 @@
+package pl.alyx.robot.sikulix.structure;
+
+public class Settings {
+
+    private boolean verbose;
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public void setVerbose(boolean verbose) {
+        this.verbose = verbose;
+    }
+
+}
